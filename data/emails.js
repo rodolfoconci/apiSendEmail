6ce8954d16cc5708fde5487871cc31d66fe72ee7
rodolfoconci <r.conci@hotmail.com>
2024-11-09 20:58:53 -0300

    nuevo template

--- data/emails.js
+++ data/emails.js
@@ -28,4 +28,17 @@ export const emailTemplates = {
         </div>
       </div>
     `,
+    bienvenidaAseguradoNuevo: (params) => `
+    <div style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px; border-radius: 8px;">
+      <div style="max-width: 600px; margin: auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);">
+        <h1 style="color: #28a745;">¡Bienvenido ${params.aseguradoName}!</h1>
+        <p style="color: #555;">Nos complace informarte que tu registro como asegurado ha sido completado exitosamente.</p>
+        <p style="color: #555;">Tu asegurador, <strong>${params.aseguradorName}</strong>, ha procesado tu regitros y pronto recibirás más detalles sobre tus pólizas.</p>
+        <p style="color: #555;">Mientras tanto, ya puedes acceder a todos nuestros servicios a través de nuestra aplicación para Android. <strong>Descárgala ahora</strong> desde Google Play para gestionar tu cuenta y estar al tanto de tus beneficios.</p>
+        <p style="color: #555;">Si tienes alguna duda o necesitas asistencia, no dudes en ponerte en contacto con tu asegurador.</p>
+        <p style="color: #555;">Te deseamos una experiencia segura y confiable con nosotros.</p>
+        <p style="color: #555;"><small>No respondas este email. Para más información, comunícate con tu asegurador o con soporte.</small></p>
+      </div>
+    </div>
+  `,
 };
