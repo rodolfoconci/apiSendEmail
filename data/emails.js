28985ad000c96302edb2f686a38f4d072d46c63f
rodolfoconci <r.conci@hotmail.com>
2024-11-10 16:43:34 -0300

    Nuevos templates

--- data/emails.js
+++ data/emails.js
@@ -28,7 +28,7 @@ export const emailTemplates = {
         </div>
       </div>
     `,
-    bienvenidaAseguradoNuevo: (params) => `
+  bienvenidaAseguradoNuevo: (params) => `
     <div style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px; border-radius: 8px;">
       <div style="max-width: 600px; margin: auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);">
         <h1 style="color: #28a745;">¡Bienvenido ${params.aseguradoName}!</h1>
@@ -41,4 +41,49 @@ export const emailTemplates = {
       </div>
     </div>
   `,
+  usuarioVerificado: (params) => `
+  <div style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px; border-radius: 8px;">
+    <div style="max-width: 600px; margin: auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);">
+      <h1 style="color: #28a745;">¡Felicidades ${params.name}!</h1>
+      <p style="color: #555;">Tu cuenta ha sido verificada exitosamente. Ahora puedes acceder a todos nuestros servicios.</p>
+      <p style="color: #555;">Gracias por tu paciencia, y te deseamos la mejor experiencia con nosotros.</p>
+      <p style="color: #555;"><small>No respondas este email.</small></p>
+    </div>
+  </div>
+`,
+  usuarioReactivado: (params) => `
+<div style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px; border-radius: 8px;">
+  <div style="max-width: 600px; margin: auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);">
+    <h1 style="color: #007bff;">Tu cuenta ha sido reactivada</h1>
+    <p style="color: #555;">Hola <strong>${params.name}</strong>,</p>
+    <p style="color: #555;">Tu cuenta ha sido reactivada después de haber sido bloqueada debido a intentos fallidos.</p>
+    <p style="color: #555;">Ya puedes acceder a tu cuenta y seguir utilizando nuestros servicios.</p>
+    <p style="color: #555;">Tu nueva contraseña es: <strong>${params.newPassword}</strong></p>
+    <p style="color: #555;">Por motivos de seguridad, te recomendamos cambiarla una vez que inicies sesión.</p>
+    <p style="color: #555;"><small>No respondas este email. Para más información, comunícate con soporte.</small></p>
+  </div>
+</div>
+`,
+  usuarioNoPago: (params) => `
+<div style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px; border-radius: 8px;">
+  <div style="max-width: 600px; margin: auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);">
+    <h1 style="color: #d9534f;">Suscripción impaga</h1>
+    <p style="color: #555;">Hola <strong>${params.name}</strong>,</p>
+    <p style="color: #555;">Tu suscripción ha sido bloqueada debido a un pago pendiente. Por favor, realiza el pago para reactivar tu cuenta.</p>
+    <p style="color: #555;">Si necesitas más detalles sobre el pago, por favor, contacta con nuestro soporte.</p>
+    <p style="color: #555;"><small>No respondas este email. Para más información, comunícate con soporte.</small></p>
+  </div>
+</div>
+`,
+  usuarioPago: (params) => `
+<div style="font-family: Arial, sans-serif; background-color: #f4f4f4; padding: 20px; border-radius: 8px;">
+  <div style="max-width: 600px; margin: auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);">
+    <h1 style="color: #28a745;">Cuenta reactivada</h1>
+    <p style="color: #555;">Hola <strong>${params.name}</strong>,</p>
+    <p style="color: #555;">Gracias por tu pago. Tu cuenta ha sido reactivada con éxito. Ya puedes acceder a todos nuestros servicios nuevamente.</p>
+    <p style="color: #555;">Si tienes alguna duda o pregunta, no dudes en ponerte en contacto con nosotros.</p>
+    <p style="color: #555;"><small>No respondas este email. Para más información, comunícate con soporte.</small></p>
+  </div>
+</div>
+`,
 };
